d6be4d4d6cd3a4e8f805c5d7b1cb49260ad59720
ByungJoon Lee <imjuni@gmail.com>
2023-10-11 04:01:56 +0900

    Fix `getGenReqIdHandler` parameter type

--- src/misc/getGenReqIdHandler.ts
+++ src/misc/getGenReqIdHandler.ts
@@ -1,11 +1,17 @@
-import type { FastifyRequest } from 'fastify';
+import type { RawRequestDefaultExpression, RawServerDefault } from 'fastify';
 import { randomUUID } from 'node:crypto';
 import path from 'node:path';
 
 export default function getGenReqIdHandler(key: string, gen?: () => string) {
   const generator = gen ?? randomUUID;
-  const genReqIdHandler = (req: Pick<FastifyRequest, 'url'>) => {
+  const genReqIdHandler = <T extends RawServerDefault>(
+    req: Pick<RawRequestDefaultExpression<T>, 'url'>,
+  ) => {
     try {
+      if (req.url == null) {
+        return generator();
+      }
+
       const url = req.url.startsWith('http')
         ? req.url
         : path.posix.join('http://localhost', req.url);
